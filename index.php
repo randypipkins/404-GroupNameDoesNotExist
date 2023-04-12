af59fc43fbc6580be794599dd3feddef3f2f0c3e
Randy Pipkins <rpipkins@ewu.edu>
2023-04-12 12:52:28 -0700

    corrected password to the server

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $username = "root";
-    $password = "CSCD350GroupWeb";
+    $password = "CSCD378GroupWeb";
 
     //Create connection
     $conn = new mysqli($servername, $username, $password);
