29b89acd716bb1dac1bc9bf3446d0f6e9bc04db4
Evelyn Barragan <ebarragan5@ewu.edu>
2023-05-04 17:37:21 -0700

    calendar changes

--- events.php
+++ events.php
@@ -12,6 +12,33 @@
   <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet">
   <!-- custom css -->
   <link rel="stylesheet" href="css/style.css" />
+  <script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.js"></script>
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+    type="text/javascript"></script>
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/locale-all.js"></script>
+<link
+    href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css"
+    rel="stylesheet">
+<link rel="stylesheet"
+    href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/form.css">
+<script src="assets/js/event.js"></script>
+<style>
+.btn-event-delete {
+    font-size: 0.85em;
+    margin: 0px 10px 0px 5px;
+    font-weight: bold;
+    color: #959595;
+}
+</style>
 </head>
 
 <body>
@@ -33,7 +60,20 @@
         <span class="mx-2 text-white">Logout</span>
       </li>
     </ul>
-  </div>
+  <!---calendar -->
+  <div class="phppot-container">
+        <h2>Event management in php</h2>
+        <div class="response"></div>
+        <div class="row">
+            <input type="text" name="filter" id="filter"
+                placeholder="Choose date" />
+            <button type="button" id="button-filter"
+                onClick="filterEvent();">Filter</button>
+        </div>
+        <div class="row">
+            <div id='calendar'></div>
+        </div>
+    </div>
   <!-- Main Wrapper -->
   <div class="p-1 my-container active-cont">
     <!-- Top Nav -->
--- script.js
+++ script.js
@@ -5,3 +5,126 @@ menu_btn.addEventListener("click", () => {
   sidebar.classList.toggle("active-nav");
   container.classList.toggle("active-cont");
 });
+//calendar
+$(document).ready(function() {
+	var calendar = $('#calendar').fullCalendar({
+		editable: true,
+		eventLimit: true,
+		droppable: true,
+		eventColor: "#fee9be",
+		eventTextColor: "#232323",
+		eventBorderColor: "#CCC",
+		eventResize: true,
+		header: {
+			right: 'prev, next today',
+			left: 'title',
+			center: 'listMonth, month, basicWeek, basicDay'
+		},
+		events: "ajax-endpoint/fetch-calendar.php",
+		displayEventTime: false,
+		eventRender: function(event, element) {
+			element.find(".fc-content").prepend("<span class='btn-event-delete'>X</span>");
+			element.find("span.btn-event-delete").on("click", function() {
+				if (confirm("Are you sure want to delete the event?")) {
+					deleteEvent(event);
+				}
+			});
+		},
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end, allDay) {
+			var title = prompt('Event Title:');
+
+			if (title) {
+				var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+				var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+				addEvent(title, start, end);
+
+				calendar.fullCalendar('renderEvent',
+					{
+						title: title,
+						start: start,
+						end: end,
+						allDay: allDay
+					},
+					true
+				);
+			}
+			calendar.fullCalendar('unselect');
+		},
+
+		eventClick: function(event) {
+			var title = prompt('Event edit Title:', event.title);
+			if (title) {
+				var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+				var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+				editEvent(title, start, end, event);
+			}
+		},
+		eventDrop: function(event) {
+			var title = event.title;
+			if (title) {
+				var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+				var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+				editEvent(title, start, end, event);
+			}
+		},
+		eventResize: function(event) {
+			var title = event.title;
+			if (title) {
+				var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+				var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+				editEvent(title, start, end, event);
+			}
+		}
+	});
+	$("#filter").datepicker();
+});
+function addEvent(title, start, end) {
+	$.ajax({
+		url: 'ajax-endpoint/add-calendar.php',
+		data: 'title=' + title + '&start=' + start + '&end=' + end,
+		type: "POST",
+		success: function(data) {
+			displayMessage("Added Successfully");
+		}
+	});
+}
+
+function editEvent(title, start, end, event) {
+	$.ajax({
+		url: 'ajax-endpoint/edit-calendar.php',
+		data: 'title=' + title + '&start=' + start + '&end=' + end + '&id=' + event.id,
+		type: "POST",
+		success: function() {
+			displayMessage("Updated Successfully");
+		}
+	});
+}
+
+function deleteEvent(event) {
+	$('#calendar').fullCalendar('removeEvents', event._id);
+	$.ajax({
+		type: "POST",
+		url: "ajax-endpoint/delete-calendar.php",
+		data: "&id=" + event.id,
+		success: function(response) {
+			if (parseInt(response) > 0) {
+				$('#calendar').fullCalendar('removeEvents', event.id);
+				displayMessage("Deleted Successfully");
+			}
+		}
+	});
+}
+function displayMessage(message) {
+	$(".response").html("<div class='success'>" + message + "</div>");
+	setInterval(function() { $(".success").fadeOut(); }, 5000);
+}
+
+function filterEvent() {
+	var filterVal = $("#filter").val();
+	if (filterVal) {
+		$('#calendar').fullCalendar('gotoDate', filterVal);
+		$("#filter").val("");
+	}
+}
