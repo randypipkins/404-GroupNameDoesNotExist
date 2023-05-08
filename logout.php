4cd2a958abf4fc8937edbd6cd5986745cef8ecf0
Randy Pipkins <rpipkins@ewu.edu>
2023-05-08 09:36:10 -0700

    changes

--- logout.php
+++ logout.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
+$servername = "localhost";
+$username = "root";
+$password = "CSCD378GroupWeb";
+$dbname = "mydb";
+
+//create connection to the server and the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
 
 session_unset();
 session_destroy();
