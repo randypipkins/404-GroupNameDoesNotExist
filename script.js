396a950fb1e0fc3ba9bd257a5e911def37d847f9
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-11 12:31:30 -0700

    adding events pt.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menu_btn = document.querySelector("#menu-btn");
-const sidebar = document.querySelector("#sidebar");
-const container = document.querySelector(".my-container");
+//const menu_btn = document.querySelector("#menu-btn");
+//const sidebar = document.querySelector("#sidebar");
+//const container = document.querySelector(".my-container");
 
 //menu_btn.addEventListener("click", () => {
    // sidebar.classList.toggle("active");
