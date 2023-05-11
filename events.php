deae880828a035dd36d62b438365efe84ad2a0bb
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-11 11:41:07 -0700

    add events fixing pt.1

--- events.php
+++ events.php
@@ -6,11 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>add event</title>
-   <!-- bootstrap 5 css -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
-   <!-- BOX ICONS CSS-->
-   <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
-   <!-- custom css -->
    <link rel="stylesheet" href="style.css" />
 </head>
 
