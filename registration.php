7f176ea5f30cf9a2a8cbf3c380621878a0e5f5c5
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-16 17:42:10 -0700

    fixing error

--- registration.php
+++ registration.php
@@ -41,6 +41,7 @@ if(mysqli_query($conn, $sql)){
     session_start();
     $_SESSION["success_msg"] = "Registration Success";
     $_SESSION["user_role"] = $user_role; 
+    $_SESSION["email"] = $email;
     header("Location: /404-GroupNameDoesNotExist");
     exit;
 } else{
