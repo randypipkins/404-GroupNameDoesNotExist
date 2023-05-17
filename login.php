4e3f19d41fd7c5c0070148b808c55e5611893014
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-16 19:37:58 -0700

    changes

--- login.php
+++ login.php
@@ -19,38 +19,32 @@ if(!isset($_POST["email"], $_POST["passwrd"])){
 }
 
 //prepare sql to prevent sql injection
-if ($stmt = $conn->prepare("SELECT id, passwrd, user_role FROM users WHERE email = ?")) {
+if($stmt = $conn->prepare("SELECT id, passwrd FROM users WHERE email = ?")){
     $stmt->bind_param('s', $_POST["email"]);
     $stmt->execute();
+    //store results to check if exists in db
     $stmt->store_result();
 
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $passwrd, $user_role);
+    if($stmt->num_rows > 0){
+        $stmt->bind_result($id, $passwrd);
         $stmt->fetch();
-        // Verify the password
-        if (password_verify($_POST["passwrd"], $passwrd)) {
-            if ($user_role === "participant") {
-                $_SESSION["loggedin"] = true;
-                $_SESSION["email"] = $_POST["email"];
-                $_SESSION["id"] = $id;
-                $_SESSION["user_role"] = $user_role;
-                header("Location: events.php");
-                exit();
-            } else {
-                // User role is not participant
-                echo "You are not authorized to login.";
-            }
-        } else {
-            // Incorrect password
+        //verify the password
+        if(password_verify($_POST["passwrd"], $passwrd)){
+            session_regenerate_id();
+            $_SESSION["loggedin"] = true;
+            $_SESSION["email"] = $_POST["email"];
+            $_SESSION["id"] = $id;
+            header("Location: eventOrg.php");
+            exit();
+        } else{
+            //incorrect password
             echo "Incorrect password.";
         }
-    } else {
-        // Incorrect email
+    } else{
+        //incorrect email
         echo "Incorrect username";
     }
 
     $stmt->close();
 }
-
-$conn->close();
 ?>
