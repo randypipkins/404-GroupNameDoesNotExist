a5d40b8ca84607333011967ffb708336e78e9678
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-18 21:33:41 -0700

    search bar

--- participant.php
+++ participant.php
@@ -12,10 +12,37 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Fetch data from the events table
-    $sql = "SELECT * FROM events";
-    $result = $conn->query($sql);
-?>
+       // Fetch data from the events table with filters
+       $search_date = $_POST['search_date'] ?? '';
+       $search_location = $_POST['search_location'] ?? '';
+       $search_capacity = $_POST['search_capacity'] ?? '';
+       $search_keywords = $_POST['search_keywords'] ?? '';
+   
+       // Construct the base SQL query
+       $sql = "SELECT * FROM events WHERE 1=1";
+   
+       // Add filters based on the search criteria
+       if (!empty($search_date)) {
+           $sql .= " AND date = '$search_date'";
+       }
+       if (!empty($search_location)) {
+           $sql .= " AND location = '$search_location'";
+       }
+       if (!empty($search_capacity)) {
+           $sql .= " AND capacity >= $search_capacity";
+       }
+       if (!empty($search_keywords)) {
+           // Split the keywords into an array
+           $keywords = explode(" ", $search_keywords);
+           $conditions = [];
+           foreach ($keywords as $keyword) {
+               $conditions[] = "title LIKE '%$keyword%' OR description LIKE '%$keyword%'";
+           }
+           $sql .= " AND (" . implode(" OR ", $conditions) . ")";
+       }
+   
+       $result = $conn->query($sql);
+   ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -51,6 +78,31 @@
   <div class="container">
     <main>
         <div class="tab tab-1" id="participantT">
+        <form method="POST" action="">
+                    <div class="row mb-3">
+                        <div class="col-md-3">
+                            <label for="search_date" class="form-label">Search by Date:</label>
+                            <input type="date" class="form-control" id="search_date" name="search_date" value="<?php echo $search_date; ?>">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="search_location" class="form-label">Search by Location:</label>
+                            <input type="text" class="form-control" id="search_location" name="search_location" value="<?php echo $search_location; ?>">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="search_capacity" class="form-label">Search by Capacity:</label>
+                            <input type="number" class="form-control" id="search_capacity" name="search_capacity" value="<?php echo $search_capacity; ?>">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="search_keywords" class="form-label">Search by Keywords:</label>
+                            <input type="text" class="form-control" id="search_keywords" name="search_keywords" value="<?php echo $search_keywords; ?>">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary">Search</button>
+                        </div>
+                    </div>
+                </form>
             <table id="table" border="1">
                 <tr>
                     <th>ID</th>
