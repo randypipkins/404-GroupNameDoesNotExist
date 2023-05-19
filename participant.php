7481d5841373fd4ebc43e0ca3a1d59f38632b523
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-19 11:57:33 -0700

    date fix

--- participant.php
+++ participant.php
@@ -23,7 +23,7 @@
    
        // Add filters based on the search criteria
        if (!empty($search_string)) {
-        $sql .= " AND date = '$search_string'";
+        $sql .= " AND DATE_FORMAT(date, '%Y-%m-%d') = '$search_string'";
     }
        if (!empty($search_location)) {
            $sql .= " AND location = '$search_location'";
