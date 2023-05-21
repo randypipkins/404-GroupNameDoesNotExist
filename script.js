92c9822fd4bebdf1a2093c1191a40c5349d0f5fd
randypipkins <rpipkins@ewu.edu>
2023-05-21 13:17:54 -0700

    work event creation

--- script.js
+++ script.js
@@ -100,13 +100,14 @@ document.querySelector(".add-btn").addEventListener("click", function(){
     var date = document.getElementById("input-3").value;
     var start_time = document.getElementById("input-4").value;
     var end_time = document.getElementById("input-5").value;
-    var capacity = document.getElementById("input-7");
+    var capacity = document.getElementById("input-7").value;
     var description = document.getElementById("input-2").value;
 
     //create new FormData object
     var formData = new FormData();
     formData.append("title", title);
     formData.append("location", location);
+    formData.append("date", date);
     formData.append("start_time", start_time);
     formData.append("end_time", end_time);
     formData.append("capacity", capacity);
@@ -118,15 +119,25 @@ document.querySelector(".add-btn").addEventListener("click", function(){
     //open a POST request to the event_handler.php file
     xhr.open("POST", "event_handler.php", true);
 
+    //set the onload event handler
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            //request was successful, handle the response
+            console.log(xhr.responseText);
+            //reset the form
+            document.getElementById("input-0").value = "";
+            document.getElementById("input-6").value = "";
+            document.getElementById("input-3").value = "";
+            document.getElementById("input-4").value = "";
+            document.getElementById("input-5").value = "";
+            document.getElementById("input-7").value = "";
+            document.getElementById("input-2").value = "";
+        } else{
+            //request failed, handle the error
+            console.error("Request failed. Status: " + xhr.status);
+        }
+    };
+
     //send the form data
     xhr.send(formData);
-
-    //reset the form
-    document.getElementById("input-0").value = "";
-    document.getElementById("input-6").value = "";
-    document.getElementById("input-3").value = "";
-    document.getElementById("input-4").value = "";
-    document.getElementById("input-5").value = "";
-    document.getElementById("input-7").value = "";
-    document.getElementById("input-2").value = "";
 });
