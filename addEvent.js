5ac7b3876e6c4259550add8c3bdb270bb6fae8b3
randypipkins <rpipkins@ewu.edu>
2023-05-21 16:53:35 -0700

    changes

--- addEvent.js
+++ addEvent.js
@@ -2,12 +2,12 @@
 document.querySelector(".add-btn").addEventListener("click", function(){
     //retrieve the form values
     var title = document.getElementById("input-0").value;
-    var location = document.getElementById("input-6").value;
-    var date = document.getElementById("input-3").value;
-    var start_time = document.getElementById("input-4").value;
-    var end_time = document.getElementById("input-5").value;
-    var capacity = document.getElementById("input-7").value;
-    var description = document.getElementById("input-2").value;
+    var location = document.getElementById("input-1").value;
+    var date = document.getElementById("input-2").value;
+    var start_time = document.getElementById("input-3").value;
+    var end_time = document.getElementById("input-4").value;
+    var capacity = document.getElementById("input-5").value;
+    var description = document.getElementById("input-6").value;
 
     //create new FormData object
     var formData = new FormData();
@@ -32,12 +32,12 @@ document.querySelector(".add-btn").addEventListener("click", function(){
             console.log(xhr.responseText);
             //reset the form
             document.getElementById("input-0").value = "";
-            document.getElementById("input-6").value = "";
+            document.getElementById("input-1").value = "";
+            document.getElementById("input-2").value = "";
             document.getElementById("input-3").value = "";
             document.getElementById("input-4").value = "";
             document.getElementById("input-5").value = "";
-            document.getElementById("input-7").value = "";
-            document.getElementById("input-2").value = "";
+            document.getElementById("input-6").value = "";
         } else{
             //request failed, handle the error
             console.error("Request failed. Status: " + xhr.status);
