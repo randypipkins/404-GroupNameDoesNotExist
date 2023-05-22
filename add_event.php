6e07580ce6cff96c3137f595759c41127b514d12
randypipkins <rpipkins@ewu.edu>
2023-05-21 20:10:25 -0700

    changes

--- add_event.php
+++ add_event.php
@@ -12,13 +12,30 @@ if (isset($_POST['add_event'])) {
     $location = $_POST['location'];
     $capacity = $_POST['capacity'];
 
-    $sql = "INSERT INTO events (title, event_type, description, date, start_time, end_time, location, capacity) 
-            VALUES ('$title', '$event_type', '$description', '$date', '$start_time', '$end_time', '$location', '$capacity')";
+    // Retrieve the email of the logged-in user from the session or authentication data
+    $logged_in_email = $_SESSION['email']; // Replace with your specific session variable or authentication data
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Event added successfully";
+    // Get the user ID based on the logged-in email
+    $query = "SELECT id FROM users WHERE email = '$logged_in_email'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $user_id = $row['id'];
+
+        // Get the organizer_id of the logged-in user
+        $organizer_id = $conn->real_escape_string($user_id);
+
+        $sql = "INSERT INTO events (title, event_type, description, date, start_time, end_time, location, capacity, organizer_id) 
+                VALUES ('$title', '$event_type', '$description', '$date', '$start_time', '$end_time', '$location', '$capacity', '$organizer_id')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Event added successfully";
+        } else {
+            echo "Error adding event: " . $conn->error;
+        }
     } else {
-        echo "Error adding event: " . $conn->error;
+        echo "User not found";
     }
 }
 
