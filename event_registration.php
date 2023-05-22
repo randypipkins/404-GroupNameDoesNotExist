eee4b51affa7c4f8118296c095ed5b97c21a329d
randypipkins <rpipkins@ewu.edu>
2023-05-22 14:20:00 -0700

    add event registration, modify participant

---
+++ event_registration.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    require_once 'config.php';
+
+    //sanitize form data
+    $event_id = mysqli_real_escape_string($conn, $_POST['event_id']);
+
+    //retrieve the logged in user's id from the session using their email
+    $user_email = $_SESSION['user_email'];
+    $sql = "SELECT id FROM users WHERE email = '$user_email'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $user_id = $row['id'];
+
+    //insert the user's participation record into the participation table in the database
+    $sql = "INSERT INTO participation (participant_capacity, wait_list, isFull, user_id, event_id) 
+        VALUES ('', '', '', '$user_id', '$event_id')";
+    $result = mysqli_query($conn, $sql);
+
+    //update the event's capacity in the events table
+    $sql = "UPDATE events SET capacity = capacity - 1 WHERE id = '$event_id'";
+    $result = mysqli_query($conn, $sql);
+
+    //display a message to the user indicating whether the registration was successful or not
+    if($result){
+        echo "Registration successful!";
+    } else{
+        echo "Registration failed: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+
+?>
