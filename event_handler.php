3b57e1c46f6d9d2b884dcefdf9f1d5fb5977cb0a
randypipkins <rpipkins@ewu.edu>
2023-05-21 18:54:42 -0700

    changes

--- event_handler.php
+++ event_handler.php
@@ -34,118 +34,69 @@ if(isset($_SESSION["email"])){
     $stmt->close();
 }
 
-//create associative array from the $_POST array
-$eventData = array(
-    'title' => $_POST['title'],
-    'location' => $_POST['location'],
-    'date' => $_POST['date'],
-    'start_time' => $_POST['start_time'],
-    'end_time' => $_POST['end_time'],
-    'capacity' => $_POST['capacity'],
-    'description' => $_POST['description'],
-    'organizer_id' => $organizer_id
-);
+// Retrieve the value of the "action" field from the $_POST array
+$action = $_POST['action'];
 
-//event construction
-class Event{
-    public $id;
-    public $title;
-    public $location;
-    public $date;
-    public $start_time;
-    public $end_time;
-    public $capacity;
-    public $description;
-    public $organizer_id;
+switch ($action) {
+  case 'add':
+    // Retrieve user input values from the $_POST array
+    $title = $_POST['title'];
+    $location = $_POST['location'];
+    $date = $_POST['date'];
+    $start_time = $_POST['start_time'];
+    $end_time = $_POST['end_time'];
+    $capacity = $_POST['capacity'];
+    $description = $_POST['description'];
 
-    public function __construct($eventData){
-        $this->title = $eventData["title"];
-        $this->location = $eventData["location"];
-        $this->date = $eventData["date"];
-        $this->start_time = $eventData["start_time"];
-        $this->end_time = $eventData["end_time"];
-        $this->capacity = $eventData["capacity"];
-        $this->description = $eventData["description"];
-        $this->organizer_id = $eventData["organizer_id"];
-    }
-}
+    // Insert the data into the "events" table using an INSERT INTO statement
+    $sql = "INSERT INTO events (title, location, date, start_time, end_time, capacity, description, organizer_id) 
+    VALUES ('$title', '$location', '$date', '$start_time', '$end_time', '$capacity', '$description', '1')";
 
-//event management system class
-class EventManagementSystem{
-    public $events;
-
-    public function __construct(){
-        $this->events = array();
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
-    //add event
-    public function addEvent($event, $organizer_id, $conn){
+    break;
+    
+  case 'edit':
+    // Retrieve user input values from the $_POST array
+    $event_id = $_POST['event_id'];
+    $title = $_POST['title'];
+    $location = $_POST['location'];
+    $date = $_POST['date'];
+    $start_time = $_POST['start_time'];
+    $end_time = $_POST['end_time'];
+    $capacity = $_POST['capacity'];
+    $description = $_POST['description'];
 
-        // Prepare and execute SQL statement to prevent injection
-        $stmt = $conn->prepare("INSERT INTO `events` (`title`, `location`, `date`, `start_time`, `end_time`, `capacity`, 
-            `description`, `organizer_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    
-        if (!$stmt) {
-            // Error occurred while preparing the statement
-            $errorLogDirectory = "ErrorLogs";
-            $errorLogFile = "error.log";
-            $errorLogPath = $errorLogDirectory . "/" . $errorLogFile;
-    
-            if (!is_dir($errorLogDirectory)) {
-                // Create the error log directory if it doesn't exist
-                mkdir($errorLogDirectory, 0755, true);
-                // Set appropriate write permissions for the directory
-                chmod($errorLogDirectory, 0755);
-            }
-    
-            if (!file_exists($errorLogPath)) {
-                // Create the error log file if it doesn't exist
-                touch($errorLogPath);
-                // Set appropriate write permissions for the file
-                chmod($errorLogPath, 0644);
-            }
-    
-            $errorMessage = "Error preparing the statement: " . $conn->error;
-            error_log($errorMessage, 3, $errorLogPath);
-            die("An error occurred. Please check the error log for details.");
-        }
-    
-        $stmt->bind_param("sssssssi", $event->title, $event->location, $event->date, $event->start_time, $event->end_time, $event->capacity, $event->description, $event->organizer_id);
-    
-        if (!$stmt->execute()) {
-            // Error executing the statement
-            $errorLogPath = "ErrorLogs/error.log";
-            $errorMessage = "Error executing the statement: " . $conn->error;
-            error_log($errorMessage, 3, $errorLogPath);
-            die("An error occurred. Please check the error log for details.");
-        }
-    
-        $event->id = $stmt->insert_id;
-    
-        $this->events[] = $event;
-    
-        $stmt->close();
+    // Update the data in the "events" table using an UPDATE statement
+    $sql = "UPDATE events SET title='$title', location='$location', date='$date', start_time='$start_time', end_time='$end_time', capacity='$capacity', description='$description' WHERE id='$event_id'";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "Record updated successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
     }
+
+    break;
+    
+  case 'delete':
+    // Retrieve the event ID from the $_POST array
+    $event_id = $_POST['event_id'];
+
+    // Delete the data from the "events" table using a DELETE statement
+    $sql = "DELETE FROM events WHERE id='$event_id'";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "Record deleted successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    break;
 }
 
-//handle the incoming request
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $title = $_POST["title"];
-    $location = $_POST["location"];
-    $date = $_POST["date"];
-    $start_time = $_POST["start_time"];
-    $end_time = $_POST["end_time"];
-    $capacity = $_POST["capacity"];
-    $description = $_POST["description"];
-
-    $event = new Event($eventData);
-
-    $eventManagementSystem = new EventManagementSystem();
-
-    //add the event
-    $eventManagementSystem->addEvent($event, $organizer_id, $conn);
-
-    //echo the event ID as the response
-    echo $event->id;
-}
+$conn->close();
 ?>
