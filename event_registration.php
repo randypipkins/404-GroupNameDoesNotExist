516fb889b0e778ba5956273418bebcf9aaf859f0
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-22 18:41:18 -0700

    popup changes

--- event_registration.php
+++ event_registration.php
@@ -2,32 +2,35 @@
     session_start();
     require_once 'config.php';
 
-    //sanitize form data
+    // Sanitize form data
     $event_id = mysqli_real_escape_string($conn, $_POST['event_id']);
 
-    //retrieve the logged in user's id from the session using their email
+    // Retrieve the logged in user's id from the session using their email
     $user_email = $_SESSION['email'];
     $sql = "SELECT id FROM users WHERE email = '$user_email'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
     $user_id = $row['id'];
 
-    //insert the user's participation record into the participation table in the database
+    // Insert the user's participation record into the participation table in the database
     $sql = "INSERT INTO participation (participant_capacity, wait_list, isFull, user_id, event_id) 
         VALUES (0, 0, false, '$user_id', '$event_id')";
     $result = mysqli_query($conn, $sql);
 
-    //update the event's capacity in the events table
+    // Update the event's capacity in the events table
     $sql = "UPDATE events SET capacity = capacity - 1 WHERE id = '$event_id'";
     $result = mysqli_query($conn, $sql);
 
-    //display a message to the user indicating whether the registration was successful or not
-    if($result){
-        echo "Registration successful!";
-    } else{
-        echo "Registration failed: " . mysqli_error($conn);
+    // Set the registration status message in a session variable
+    if ($result) {
+        $_SESSION['registration_status'] = "Registration successful!";
+    } else {
+        $_SESSION['registration_status'] = "Registration failed: " . mysqli_error($conn);
     }
 
     mysqli_close($conn);
 
+    // Redirect back to the participant.php page
+    header("Location: participant.php");
+    exit();
 ?>
