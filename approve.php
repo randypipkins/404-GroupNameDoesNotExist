62eed020bae5da03e3ab99324477f0fa0b8f5295
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-23 15:05:32 -0700

    trying to fix error

--- approve.php
+++ approve.php
@@ -15,23 +15,34 @@ if (isset($_POST['event_id'])) {
     if ($result && $result->num_rows > 0) {
         $event = $result->fetch_assoc();
 
-        // Insert the event data into the approved_events table
-        $title = $event['title'];
-        $event_type = $event['event_type'];
-        $location = $event['location'];
-        $date = $event['date'];
-        $start_time = $event['start_time'];
-        $end_time = $event['end_time'];
-        $capacity = $event['capacity'];
-        $description = $event['description'];
+        // Get the organizer ID
         $organizer_id = $event['organizer_id'];
 
-        $approved_sql = "INSERT INTO approved_events (title, event_type, location, date, start_time, end_time, capacity, description, organizer_id) VALUES ('$title', '$event_type', '$location', '$date', '$start_time', '$end_time', '$capacity', '$description', $organizer_id)";
+        // Check if the organizer ID exists in the users table
+        $check_organizer_sql = "SELECT * FROM users WHERE id = $organizer_id";
+        $check_organizer_result = $conn->query($check_organizer_sql);
 
-        if ($conn->query($approved_sql) === TRUE) {
-            echo "Event approved successfully";
+        if ($check_organizer_result && $check_organizer_result->num_rows > 0) {
+            // Organizer ID exists, proceed with inserting the event data into the approved_events table
+
+            $title = $event['title'];
+            $event_type = $event['event_type'];
+            $location = $event['location'];
+            $date = $event['date'];
+            $start_time = $event['start_time'];
+            $end_time = $event['end_time'];
+            $capacity = $event['capacity'];
+            $description = $event['description'];
+
+            $approved_sql = "INSERT INTO approved_events (title, event_type, location, date, start_time, end_time, capacity, description, organizer_id) VALUES ('$title', '$event_type', '$location', '$date', '$start_time', '$end_time', '$capacity', '$description', $organizer_id)";
+
+            if ($conn->query($approved_sql) === TRUE) {
+                echo "Event approved successfully";
+            } else {
+                echo "Error approving event: " . $conn->error;
+            }
         } else {
-            echo "Error approving event: " . $conn->error;
+            echo "Invalid organizer ID";
         }
     } else {
         echo "Event not found";
