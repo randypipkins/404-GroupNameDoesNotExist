90de2f1738e053b4d7e30851492d894c59e16384
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-23 10:20:45 -0700

    user changes

--- users.php
+++ users.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "CSCD378GroupWeb";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
