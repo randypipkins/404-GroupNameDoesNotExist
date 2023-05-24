5bb17fca1202d9e554c1be595866af5fff6742aa
randypipkins <rpipkins@ewu.edu>
2023-05-24 11:53:28 -0700

    changes for testing

--- delete_event.php
+++ delete_event.php
@@ -1,8 +1,19 @@
 <?php
 // deleteEvent.php
 
-// Establish database connection
-require_once("config.php");
+// Database configuration
+$host = "localhost"; // Hostname
+$username = "root"; // Database username
+$password = "CSCD378GroupWeb"; // Database password
+$dbname = "mydb"; // Database name
+
+// Create a database connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Check if the event ID is provided via POST
 if (isset($_POST['event_id'])) {
