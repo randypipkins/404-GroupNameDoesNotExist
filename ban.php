e3a446fb14931608dccc4b6db014bc957d148280
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-26 19:31:19 -0700

    ban.php

---
+++ ban.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "CSCD378GroupWeb";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['user_id'])) {
+    $userId = $_POST['user_id'];
+
+    // Update the 'is_banned' column in the database for the selected user
+    $banQuery = "UPDATE users SET is_banned = 1 WHERE id = $userId";
+    if ($conn->query($banQuery) === TRUE) {
+        echo "<span class='text-success'>User banned</span>";
+    } else {
+        echo "<span class='text-danger'>Error banning user: " . $conn->error . "</span>";
+    }
+}
+?>
