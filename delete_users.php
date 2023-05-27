95ff8455ef3091f7df0069fabde4303d505a3c78
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-26 19:54:20 -0700

    fixing error

--- delete_users.php
+++ delete_users.php
@@ -16,18 +16,12 @@ if ($conn->connect_error) {
 if (isset($_POST['user_id'])) {
     $userId = $_POST['user_id'];
 
-    // Delete the event from the events table
-    $delete_sql = "DELETE FROM users WHERE id = $user_id";
-
-    if ($conn->query($delete_sql) === TRUE) {
-        echo "user deleted successfully";
+    // Update the 'is_banned' column in the database for the selected user
+    $DeleteQuery = "DELETE FROM users WHERE id = $userId";
+    if ($conn->query($DeleteQuery) === TRUE) {
+        echo "<span class='text-success'>User Deleted</span>";
     } else {
-        echo "Error deleting user: " . $conn->error;
+        echo "<span class='text-danger'>Error deleting user: " . $conn->error . "</span>";
     }
-} else {
-    echo "Invalid request";
 }
-
-// Close the database connection
-$conn->close();
 ?>
