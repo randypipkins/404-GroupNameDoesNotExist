eddebc341f0aa2db321b0b6806358106f9895810
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-29 18:47:17 -0700

    changes

--- events.php
+++ events.php
@@ -1,44 +1,5 @@
-<?php
-// events.php
-
-// Database configuration
-$host = "localhost";
-$username = "root";
-$password = "CSCD378GroupWeb";
-$dbname = "mydb";
-
-// Create a database connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Retrieve events from the database
-$select_sql = "SELECT * FROM events";
-$result = $conn->query($select_sql);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Add Event</title>
-  <!-- bootstrap 5 css -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
-  <!-- BOX ICONS CSS-->
-  <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
-  <!-- custom css -->
-  <link rel="stylesheet" href="css/style.css" />
-</head>
-
 <body>
-    <div class="wrapper">
+    <!-- Side-Nav -->
     <div class="side-nav active-nav" id="sidebar">
         <ul class="nav flex-column text-white w-100">
             <h3 class="h3 text-white my-2" id="h3"> I Do Crew </h3>
@@ -59,11 +20,10 @@ $result = $conn->query($select_sql);
             </li>
         </ul>
     </div>
-
     <div class="container">
-    <main>
-    <div class="tab tab-1" id="participantT">
-            <table id="table" border="1">
+        <main>
+            <div class="tab tab-1">
+                <table id="table" border="1">
                     <tr>
                         <th>Event Title</th>
                         <th>Event Type</th>
@@ -73,40 +33,49 @@ $result = $conn->query($select_sql);
                         <th>End Time</th>
                         <th>Location</th>
                         <th>Capacity</th>
-                        <th>Organizer ID</th>
-                        <th>Remove</th>
+                        <th>Options</th>
                     </tr>
 
                     <?php
-                // Fetch data from the events table
-                if ($result && $result->num_rows > 0) {
-                    // Output data of each row
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row["id"] . "</td>";
-                        echo "<td>" . $row["title"] . "</td>";
-                        echo "<td>" . $row["location"] . "</td>";
-                        echo "<td>" . $row["date"] . "</td>";
-                        echo "<td>" . $row["start_time"] . "</td>";
-                        echo "<td>" . $row["end_time"] . "</td>";
-                        echo "<td>" . $row["capacity"] . "</td>";
-                        echo "<td>" . $row["description"] . "</td>";
-                        echo "<td>" . $row["organizer_id"] . "</td>";
-                        echo "<td>";
-                        echo "<form method='POST' action='delete_event.php'>"; 
-                        echo "<input type='hidden' name='event_id' value='" . $row["id"] . "'>";
-                        echo "<button type='submit' class='btn btn-primary'>Remove</button>";
-                        echo "</form>";
-        
-        echo "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "<tr><td colspan='12'>No events found</td></tr>";
-}
-?>
-</table>
-</div>
+                    // Display the events in the table
+                    if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){
+                            $event_id = $row['id'];
+                            $event_title = $row['title'];
+                            $event_type = $row['event_type'];
+                            $event_location = $row['location'];
+                            $event_date = $row['date'];
+                            $event_start_time = $row['start_time'];
+                            $event_end_time = $row['end_time'];
+                            $event_capacity = $row['capacity'];
+                            $event_description = $row['description'];
+                    ?>
+
+                    <tr>
+                        <td><?php echo $event_title; ?></td>
+                        <td><?php echo $event_type; ?></td>
+                        <td><?php echo $event_location; ?></td>
+                        <td><?php echo $event_date; ?></td>
+                        <td><?php echo $event_start_time; ?></td>
+                        <td><?php echo $event_end_time; ?></td>
+                        <td><?php echo $event_capacity; ?></td>
+                        <td><?php echo $event_description; ?></td>
+                        <td>
+                            <form action="delete_event.php" method="POST" style="display: inline;">
+                                <input type="hidden" name="event_id" value="<?php echo $event_id; ?>">
+                                <button type="submit" name="delete_event">Delete</button>
+                            </form>
+                            <a href="">Modify</a> <!--Modify when modify_event.php is completed-->
+                        </td>
+                    </tr>
+                    <?php
+                        }
+                    } else{
+                        echo "<tr><td colspan='9'>No events found</td></tr>";
+                    }
+                    ?>
+                </table>
+            </div>
 
             <div class="tab tab-2">
                     <label for="event-title">Event Title:</label>
@@ -138,11 +107,11 @@ $result = $conn->query($select_sql);
                     <div class="button-div">
                         <button class="btn add-btn" name="add_event">Add</button>
                         <button class="btn edit-btn">Edit</button>
+                        <button class="btn remove-btn" onclick="confirmDeleteEvent()">Remove</button>
                     </div>
             </div>
         </main>
     </div>
-</div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <script src="script.js" async defer></script>
