a23d2475eae4ada9708553d89c1aabdcf76996d8
randypipkins <rpipkins@ewu.edu>
2023-05-30 13:45:08 -0700

    changes

--- modify_event.php
+++ modify_event.php
@@ -1,4 +1,18 @@
 <?php
+// Database configuration
+$host = "localhost";
+$username = "root";
+$password = "CSCD378GroupWeb";
+$dbname = "mydb";
+
+// Create a database connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 // Check if the event ID is provided in the query parameter
 if (!isset($_GET['event_id'])) {
     echo "Event ID not provided";
