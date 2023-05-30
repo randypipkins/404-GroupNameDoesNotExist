011d2b12c060e547089b60ec27a421d6d28c8ef7
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-29 18:21:34 -0700

    trying to fix error

--- script.js
+++ script.js
@@ -91,39 +91,40 @@ function editRow() {
 
 function removeRow() {
     if (table.rows.length > 1) {
-        table.deleteRow(rIndex);
+        const selectedRow = table.rows[rIndex]; // Assuming rIndex is defined and contains the index of the selected row
         const inputs = document.querySelectorAll('input[type="text"]');
         inputs.forEach((input) => {
             input.value = "";
         });
-            // Send the data to the server
-            const data = new FormData();
-            data.append("title", selectedRow.cells[0].innerHTML);
-            data.append("event_type", selectedRow.cells[1].innerHTML);
-            data.append("description", selectedRow.cells[2].innerHTML);
-            data.append("date", selectedRow.cells[3].innerHTML);
-            data.append("start_time", selectedRow.cells[4].innerHTML);
-            data.append("end_time", selectedRow.cells[5].innerHTML);
-            data.append("location", selectedRow.cells[6].innerHTML);
-            data.append("capacity", selectedRow.cells[7].innerHTML);
-        
-            const xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        alert("Event removed successfully!");
-                        selectedRow.remove(); // Remove the row from the table
-                    } else {
-                        alert("Error removing event: " + xhr.responseText);
-                    }
+
+        // Send the data to the server
+        const data = new FormData();
+        data.append("title", selectedRow.cells[0].innerHTML);
+        data.append("event_type", selectedRow.cells[1].innerHTML);
+        data.append("description", selectedRow.cells[2].innerHTML);
+        data.append("date", selectedRow.cells[3].innerHTML);
+        data.append("start_time", selectedRow.cells[4].innerHTML);
+        data.append("end_time", selectedRow.cells[5].innerHTML);
+        data.append("location", selectedRow.cells[6].innerHTML);
+        data.append("capacity", selectedRow.cells[7].innerHTML);
+
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    alert("Event removed successfully!");
+                    selectedRow.remove(); // Remove the row from the table
+                } else {
+                    alert("Error removing event: " + xhr.responseText);
                 }
-            };
-        
-            xhr.open("POST", "remove_event.php", true);
-            xhr.send(data);
-        }
-        
+            }
+        };
+
+        xhr.open("POST", "remove_event.php", true);
+        xhr.send(data);
     }
+}
+
 
 // Event Listeners
 addBtn.addEventListener("click", () => {
