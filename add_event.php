896d4c36ed3d205346455bbaba8fd7de5d985e62
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-30 20:10:35 -0700

    chnages

--- add_event.php
+++ add_event.php
@@ -5,15 +5,15 @@ require_once 'config.php';
 // Start the session
 session_start();
 
-if (isset($_POST['title']) && isset($_POST['event_type']) && isset($_POST['description']) && isset($_POST['date']) && isset($_POST['start_time']) && isset($_POST['end_time']) && isset($_POST['location']) && isset($_POST['capacity'])) {
+if (isset($_POST['title']) && isset($_POST['event_type']) && isset($_POST['location']) && isset($_POST['date']) && isset($_POST['start_time']) && isset($_POST['end_time']) && isset($_POST['capacity']) && isset($_POST['description']) ) {
     $title = $_POST['title'];
     $event_type = $_POST['event_type'];
-    $description = $_POST['description'];
+    $location = $_POST['location'];
     $date = $_POST['date'];
     $start_time = $_POST['start_time'];
     $end_time = $_POST['end_time'];
-    $location = $_POST['location'];
     $capacity = $_POST['capacity'];
+    $description = $_POST['description'];
 
     // Retrieve the email of the logged-in user from the session or authentication data
     $logged_in_email = $_SESSION['email']; // Replace with your specific session variable or authentication data
@@ -35,8 +35,8 @@ if (isset($_POST['title']) && isset($_POST['event_type']) && isset($_POST['descr
         // Get the organizer_id of the logged-in user
         $organizer_id = $conn->real_escape_string($user_id);
 
-        $sql = "INSERT INTO events (title, event_type, description, date, start_time, end_time, location, capacity, organizer_id) 
-                VALUES ('$title', '$event_type', '$description', '$date', '$start_time', '$end_time', '$location', '$capacity', '$organizer_id')";
+        $sql = "INSERT INTO events (title, event_type, location, date, start_time, end_time, capacity, description, organizer_id) 
+                VALUES ('$title', '$event_type', '$location', '$date', '$start_time', '$end_time',  '$capacity', '$description', '$organizer_id')";
 
         // Retrieve the event_id for the newly inserted event
         $event_id = $conn->insert_id;
