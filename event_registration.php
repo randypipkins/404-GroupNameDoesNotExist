2778b8bc4ac5b7c1005f679f424a8f4664167af9
Evebarr20 <Ebarragan5@ewu.edu>
2023-05-30 18:21:21 -0700

    wait_list feature

--- event_registration.php
+++ event_registration.php
@@ -13,16 +13,17 @@
     $user_id = $row['id'];
 
     // Check if the event has reached its capacity
-    $event_query = "SELECT capacity FROM approved_events WHERE id = '$event_id'";
+    $event_query = "SELECT capacity, wait_list FROM approved_events WHERE id = '$event_id'";
     $event_result = mysqli_query($conn, $event_query);
     $event_row = mysqli_fetch_assoc($event_result);
     $event_capacity = $event_row['capacity'];
+    $wait_list = $event_row['wait_list'];
 
     if ($event_capacity > 0) {
         // The event still has capacity, register the participant
 
-        // Update the event's capacity in the approved_events table
-        $sql = "UPDATE approved_events SET capacity = capacity - 1 WHERE id = '$event_id'";
+        // Update the event's capacity and participant count in the approved_events table
+        $sql = "UPDATE approved_events SET capacity = capacity - 1, participant_count = participant_count + 1 WHERE id = '$event_id'";
         $result = mysqli_query($conn, $sql);
 
         // Error checking
@@ -46,10 +47,39 @@
 
         // Display a message to the user indicating the registration was successful
         echo "Registration successful!";
+    } elseif ($wait_list > 0) {
+        // The event is at full capacity, register the user for the waitlist
+
+        // Update the waitlist count in the approved_events table
+        $sql = "UPDATE approved_events SET wait_list = wait_list - 1 WHERE id = '$event_id'";
+        $result = mysqli_query($conn, $sql);
+
+        // Error checking
+        if(!$result){
+            $error_message = $conn->error;
+            $file_name = __FILE__;
+            log_error($error_message, $file_name);
+        }
+
+        // Insert the participant's record into the participation table with waitlist information
+        $sql = "INSERT INTO participation (participant_capacity, wait_list, isFull, user_id, email, event_id) 
+                VALUES (0, 0, true, '$user_id', '$user_email', '$event_id')";
+        $result = mysqli_query($conn, $sql);
+
+        // Error checking
+        if(!$result){
+            $error_message = $conn->error;
+            $file_name = __FILE__;
+            log_error($error_message, $file_name);
+        }
+
+        // Display a message to the user indicating the waitlist registration was successful
+        echo "Registration successful! You have been added to the waitlist.";
     } else {
-        // The event is already at full capacity, inform the user
-        echo "Sorry, the event is already full and no more registrations can be accepted.";
+        // The event and waitlist are both full, inform the user
+        echo "Sorry, the event and waitlist are both full. No more registrations can be accepted.";
     }
 
     mysqli_close($conn);
 ?>
+
